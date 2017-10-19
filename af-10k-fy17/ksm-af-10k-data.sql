f51a4d90010f19276425384728f5cd3374161d8a
Paul Hively <pbhively@gmail.com>
2017-10-19 14:56:01 -0500

    Householded giving/data pull

--- af-10k-fy17/ksm-af-10k-data.sql
+++ af-10k-fy17/ksm-af-10k-data.sql
@@ -4,37 +4,43 @@ With
  Giving queries
 *************************/
 
+/* Householded transactions */
+giving_hh As (
+  Select *
+  From v_ksm_giving_trans_hh
+),
+
 /* First year of Kellogg giving */
 ksm_giving_yr As (
   Select
-    id_number
-    , min(Case When recognition_credit > 0 Then fiscal_year End) As first_year
-  From v_ksm_giving_trans
-  Group By id_number
+    household_id
+    , min(Case When hh_recognition_credit > 0 Then fiscal_year End) As first_year
+  From giving_hh
+  Group By household_id
 ),
 
 /* Total lifetime giving transactions */
 ksm_giving As (
   Select
-    v.id_number
-    , count(Distinct Case When recognition_credit > 0 Then allocation_code End) As gifts_allocs_supported
-    , count(Distinct Case When recognition_credit > 0 Then fiscal_year End) As gifts_fys_supported
-    , min(Case When recognition_credit > 0 Then fiscal_year End) As giving_first_year
+    giving_hh.household_id
+    , count(Distinct Case When hh_recognition_credit > 0 Then allocation_code End) As gifts_allocs_supported
+    , count(Distinct Case When hh_recognition_credit > 0 Then fiscal_year End) As gifts_fys_supported
+    , min(Case When hh_recognition_credit > 0 Then fiscal_year End) As giving_first_year
     , sum(Case When fiscal_year = ksm_giving_yr.first_year And tx_gypm_ind <> 'P'
-        Then recognition_credit End) As giving_first_cash_amount
+        Then hh_recognition_credit End) As giving_first_cash_amount
     , sum(Case When fiscal_year = ksm_giving_yr.first_year And tx_gypm_ind = 'P'
-        Then recognition_credit End) As giving_first_pledge_amount
-    , sum(Case When tx_gypm_ind = 'P' Then NULL Else recognition_credit End) As giving_cash_total
+        Then hh_recognition_credit End) As giving_first_pledge_amount
+    , sum(Case When tx_gypm_ind = 'P' Then NULL Else hh_recognition_credit End) As giving_cash_total
     , sum(Case When tx_gypm_ind = 'P' And pledge_status Not In ('I', 'R') -- Exclude inactive and reconciliation
-        Then recognition_credit Else NULL End) As giving_pledge_total
+        Then hh_recognition_credit Else NULL End) As giving_pledge_total
     , sum(Case When (tx_gypm_ind = 'P' And pledge_status Not In ('I', 'R')) Or tx_gypm_ind In ('G', 'M')
-        Then recognition_credit Else NULL End) As giving_ngc_total
+        Then hh_recognition_credit Else NULL End) As giving_ngc_total
     , sum(Case When payment_type = 'Cash / Check' And tx_gypm_ind <> 'M' Then 1 End) As gifts_cash
     , sum(Case When payment_type = 'Credit Card' And tx_gypm_ind <> 'M' Then 1 End) As gifts_credit_card
     , sum(Case When payment_type = 'Securities' And tx_gypm_ind <> 'M' Then 1 End) As gifts_stock
-  From v_ksm_giving_trans v
-  Left Join ksm_giving_yr On ksm_giving_yr.id_number = v.id_number
-  Group By v.id_number
+  From giving_hh
+  Left Join ksm_giving_yr On ksm_giving_yr.household_id = giving_hh.household_id
+  Group By giving_hh.household_id
 )
 
 /*************************
@@ -43,9 +49,11 @@ ksm_giving As (
 
 Select
   -- Identifiers
-    entity.id_number
-  , entity.report_name
-  , entity.record_status_code
+    hh.id_number
+  , hh.report_name
+  , hh.record_status_code
+  , hh.household_id
+  , Case When hh.id_number = hh.household_id Then 'Y' Else 'N' End As hh_primary
   -- Dependent variables
   --, dependent variable -- ever made a $10K gift
   --, dependent variable -- ever made a $100K gift
@@ -97,5 +105,5 @@ Select
   --, Club Leader indicator
   --, number of events attended
   --, ever attended Reunion
-From entity
-Left Join ksm_giving On ksm_giving.id_number = entity.id_number
+From v_entity_ksm_households hh
+Left Join ksm_giving On ksm_giving.household_id = hh.household_id
