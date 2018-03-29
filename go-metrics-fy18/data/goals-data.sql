2d4266a486a4d944843298705e23e504f8c04da6
Paul Hively <pbhively@gmail.com>
2018-03-29 11:37:41 -0500

    Goals, performance, and tenure query

--- go-metrics-fy18/data/goals-data.sql
+++ go-metrics-fy18/data/goals-data.sql
@@ -11,34 +11,45 @@ Select
        - start_dt) / 365.25
        , 2
     ) As go_tenure_ksm_total
-  -- Tenure in GO role as of start of indicated PY, in years
+  -- Tenure in GO role as of start of indicated month, in years
   , round(
-      (to_date(goal.year - 1 || '0501', 'yyyymmdd') -- PY start is May (5)
+      (to_date(goals.cal_year || lpad(goals.cal_month, 2, '0') || '01', 'yyyymmdd') -- PY start is May (5)
        - start_dt) / 365.25
        , 2
-    ) As go_tenure_py_start
-  -- Account for GOs whose start/stop date was midway through the PY: what % of the year was at KSM
+    ) As go_tenure_mo_start
+  -- Account for GOs whose start/stop date was during/before/after the time period
   , round(
       Case
-        When start_dt > to_date(goal.year - 1 || '0501', 'yyyymmdd') Then
-          (to_date(goal.year || '0430', 'yyyymmdd') - start_dt) / 365.25
+        When start_dt > to_date(goals.cal_year || lpad(goals.cal_month, 2, '0') || '01', 'yyyymmdd') Then
+          (to_date(goals.cal_year || lpad(goals.cal_month, 2, '0') || '01', 'yyyymmdd') - start_dt) / 365.25
         When stop_dt Is Null Then 1
-        When stop_dt >= to_date(goal.year || '0430', 'yyyymmdd') Then 1
-        When stop_dt < to_date(goal.year - 1 || '0501', 'yyyymmdd') Then 0
-        Else (stop_dt - to_date(goal.year - 1 || '0501', 'yyyymmdd')) / 365.25
+        -- Start of next month - 1 day = end of current month; modulo so that 12 wraps to 1
+        -- Wasn't able to test this due to no data as of 3/29/18
+        When stop_dt >= to_date(goals.cal_year || lpad(mod(goals.cal_month, 12) + 1, 2, '0') || '01', 'yyyymmdd') - 1 Then 1
+        When stop_dt < to_date(goals.cal_year || lpad(mod(goals.cal_month, 12) + 1, 2, '0') || '01', 'yyyymmdd') - 1 Then 0
+        Else (stop_dt - to_date(goals.cal_year || lpad(mod(goals.cal_month, 12) + 1, 2, '0') || '01', 'yyyymmdd') - 1) / 365.25
       End
       , 4
-    ) As go_tenure_pct_at_ksm
-  -- Goal table fields
-  , goal.year
-  , goal.goal_1 As closes_goal
-  , goal.goal_2 As solicitations_goal
-  , goal.goal_3 As dollars_goal
-  , goal.goal_4 As visits_goal
-  , goal.goal_5 As qualifications_goal
-From goal
+    ) As go_tenure_at_ksm
+  -- Goal view fields
+  , goals.goal_type
+  , Case
+      When goals.goal_type = 'MGC' Then 'Closes'
+      When goals.goal_type = 'MGDR' Then 'Dollars'
+      When goals.goal_type = 'MGS' Then 'Solicitations'
+      When goals.goal_type = 'NOQV' Then 'Qualifications'
+      When goals.goal_type = 'NOV' Then 'Visits'
+      When goals.goal_type = 'PA' Then 'Assists'
+    End As goal_desc
+  , goals.cal_year
+  , goals.cal_month
+  , goals.perf_quarter
+  , goals.perf_year
+  , goals.goal
+  , goals.cnt
+From v_mgo_goals_monthly goals
 Cross Join v_current_calendar cal
-Inner Join mv_past_ksm_gos gos On gos.id_number = goal.id_number
+Inner Join mv_past_ksm_gos gos On gos.id_number = goals.id_number
 Order By
   report_name Asc
   , year Asc
