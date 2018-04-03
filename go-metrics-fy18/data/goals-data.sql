bbf991711ea5d466fcfbb7dd1d0cb55763d560be
Paul Hively <pbhively@gmail.com>
2018-04-03 14:52:51 -0500

    Pull goals from v_mgo_activity_monthly

--- go-metrics-fy18/data/goals-data.sql
+++ go-metrics-fy18/data/goals-data.sql
@@ -13,50 +13,40 @@ Select
     ) As go_tenure_ksm_total
   -- Tenure as an NU GO as of start of indicated month, in years
   , round(
-      (to_date(goals.cal_year || lpad(goals.cal_month, 2, '0') || '01', 'yyyymmdd') -- PY start is May (5)
+      (to_date(act.cal_year || lpad(act.cal_month, 2, '0') || '01', 'yyyymmdd') -- PY start is May (5)
        - start_dt) / 365.25
        , 2
     ) As go_tenure_mo_start
   -- Account for GOs whose start/stop date was during/before/after the time period
   , Case
-      When to_date(goals.cal_year || lpad(goals.cal_month, 2, '0') || '01', 'yyyymmdd')
+      When to_date(act.cal_year || lpad(act.cal_month, 2, '0') || '01', 'yyyymmdd')
         Between start_dt And stop_dt
         Then 1
-      When to_date(goals.cal_year || lpad(goals.cal_month, 2, '0') || '01', 'yyyymmdd') >= start_dt
+      When to_date(act.cal_year || lpad(act.cal_month, 2, '0') || '01', 'yyyymmdd') >= start_dt
         And stop_dt Is Null
         Then 1
       Else 0
     End As go_at_ksm
-  -- Goal view fields
-  , goals.goal_type
+  -- Goal/activity view fields
+  , act.goal_type
   , Case
-      When goals.goal_type = 'MGC' Then 'Closes'
-      When goals.goal_type = 'MGDR' Then 'Dollars'
-      When goals.goal_type = 'MGS' Then 'Solicitations'
-      When goals.goal_type = 'NOQV' Then 'Qualifications'
-      When goals.goal_type = 'NOV' Then 'Visits'
-      When goals.goal_type = 'PA' Then 'Assists'
+      When act.goal_type = 'MGC' Then 'Closes'
+      When act.goal_type = 'MGDR' Then 'Dollars'
+      When act.goal_type = 'MGS' Then 'Solicitations'
+      When act.goal_type = 'NOQV' Then 'Qualifications'
+      When act.goal_type = 'NOV' Then 'Visits'
+      When act.goal_type = 'PA' Then 'Assists'
     End As goal_desc
-  , goals.cal_year
-  , goals.cal_month
-  , goals.perf_quarter
-  , goals.perf_year
-  , goals.goal
-  , goals.cnt
-  -- PY goal from self join
-  , Case
-      When goals2.goal Is Not Null Then goals2.goal
-      Else goals.goal
-    End As py_goal
-From v_mgo_goals_monthly goals
+  , act.cal_year
+  , act.cal_month
+  , act.perf_year
+  , act.perf_quarter
+  , act.fy_goal
+  , act.py_goal
+  , act.adjusted_progress
+From v_mgo_activity_monthly act
 Cross Join v_current_calendar cal
-Inner Join mv_past_ksm_gos gos On gos.id_number = goals.id_number
--- Self join to add PY goal
-Left Join v_mgo_goals_monthly goals2
-  On goals2.id_number = goals.id_number
-  And goals2.goal_type = goals.goal_type
-  And goals2.perf_year = goals.year
-  And goals2.cal_month = goals.cal_month
+Inner Join mv_past_ksm_gos gos On gos.id_number = act.id_number
 Order By
   report_name Asc
   , cal_year Asc
