3c86a1c8f4f5829c7ed428283243434f7bc091eb
Paul Hively <pbhively@gmail.com>
2018-08-02 16:59:33 -0500

    PG scores: starting modeling

--- pg-cultivation-score-fy18/code/functions.R
+++ pg-cultivation-score-fy18/code/functions.R
@@ -9,10 +9,15 @@ scatterplotter <- function(data, x, y, color = NULL, ytrans = 'identity', ylabel
 }
 
 # Log10 transformation after adding 1
+log10plus1 <- function(x, inverse = FALSE) {
+  if (inverse) {return(10^x - 1)}
+  else return(log10(x + 1))
+}
+# scales::trans_new version
 log10plus1_trans <- function(x) {
   scales::trans_new(
     'log10plus1'
-    , transform = function(x) log10(x + 1)
-    , inverse = function(x) {10^x - 1}
+    , transform = function(x) log10plus1(x, inverse = FALSE)
+    , inverse = function(x) log10plus1(x, inverse = TRUE)
   )
 }
