8e84cd9550ae7ecc322c208b7ee9933b492f8390
Paul Hively <pbhively@gmail.com>
2019-04-25 16:39:18 -0500

    Interests: CR query

---
+++ prospect-interests-fy19/code/sql contact reports.sql
@@ -0,0 +1,11 @@
+Select
+  cr.id_number
+  , trim(cr.id_number_2)
+    As id_number_2
+  , cr.contact_date
+  , ksm_pkg.get_fiscal_year(cr.contact_date)
+    As contact_fy
+  , cr.description
+  , cr.summary
+From contact_report cr
+Where cr.contact_type = 'V'
