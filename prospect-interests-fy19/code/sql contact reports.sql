e6fb23774b8dc03e403115ce746fec7446b455e8
Paul Hively <pbhively@gmail.com>
2019-07-05 17:42:41 -0500

    Interests: final CR data file
    - Tweaked SQL code and cleanup code to remove hh:mm:ss timestamps from CR dates
    - The visits data frame variables now have sensible types (date, numeric)

--- prospect-interests-fy19/code/sql contact reports.sql
+++ prospect-interests-fy19/code/sql contact reports.sql
@@ -2,7 +2,8 @@ Select
   cr.id_number
   , trim(cr.id_number_2)
     As id_number_2
-  , cr.contact_date
+  , trim(cr.contact_date)
+    As contact_date
   , ksm_pkg.get_fiscal_year(cr.contact_date)
     As contact_fy
   , cr.description
