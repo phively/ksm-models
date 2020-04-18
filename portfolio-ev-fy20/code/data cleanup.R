2ca994457845d2aba8c08b38db4748cfb6bc3ea3
Paul Hively <pbhively@gmail.com>
2020-04-17 19:55:43 -0500

    Portfolio EV: entityset creation functions
    - Working generic helper functions to create entity sets filtered by cutoff date
    - Updated data cleanup script

--- portfolio-ev-fy20/code/data cleanup.R
+++ portfolio-ev-fy20/code/data cleanup.R
@@ -17,3 +17,37 @@ catracks$prs_evaluation <- catracks$prs_evaluation %>%
   filter(!is.na(EVAL_DT))
 catracks$prs_contact <- catracks$prs_contact %>%
   filter(!is.na(CONTACT_DATE))
+
+# Remove unused fields
+catracks$households <- catracks$households %>%
+  select(-HOUSEHOLD_SPOUSE_ID, -HOUSEHOLD_SPOUSE_RPT_NAME, -DATA_AS_OF)
+# Biodata
+catracks$bio_address <- catracks$bio_address %>%
+  select(-START_DT, -STOP_DT, -DATE_MODIFIED, -ADDR_TYPE_CODE)
+catracks$bio_email <- catracks$bio_email %>%
+  select(-EMAIL_TYPE_CODE, -START_DT, -STOP_DT)
+catracks$bio_employment <- catracks$bio_employment %>%
+  select(-START_DT, -STOP_DT, -JOB_TITLE, -EMPLOYER_NAME)
+catracks$bio_phone <- catracks$bio_phone %>%
+  select(-TELEPHONE_TYPE_CODE)
+# Engagement
+catracks$eng_activity <- catracks$eng_activity %>%
+  select(-ID_NUMBER, -START_DT, -STOP_DT)
+catracks$eng_committee <- catracks$eng_committee %>%
+  select(-ID_NUMBER, -COMMITTEE_CODE, -COMMITTEE_ROLE_CODE, -COMMITTEE_ROLE_XSEQUENCE)
+catracks$eng_event <- catracks$eng_event %>%
+  select(-ID_NUMBER, -EVENT_ID)
+# Giving
+catracks$giv_giftclub <- catracks$giv_giftclub %>%
+  select(-GIFT_CLUB_CODE, -GIFT_CLUB_START_DATE, -GIFT_CLUB_END_DATE, -STOP_DT)
+catracks$giv_transactions <- catracks$giv_transactions %>%
+  select(-TX_NUMBER, -TX_SEQUENCE, -MATCHED_TX_NUMBER, -PLEDGE_NUMBER)
+# Prospect
+catracks$prs_assignment <- catracks$prs_assignment %>%
+  select(-ASSIGNMENT_TYPE, COMMITTEE_DESC)
+catracks$prs_contact <- catracks$prs_contact %>%
+  select(-CONTACT_CREDIT_TYPE)
+catracks$prs_evaluation <- catracks$prs_evaluation %>%
+  select(-EVALUATION_TYPE, -RATING_CODE)
+catracks$prs_program <- catracks$prs_program %>%
+  select(-PROGRAM_CODE, -START_DATE, -STOP_DATE)
--- portfolio-ev-fy20/code/featuretools helper functions.R
+++ portfolio-ev-fy20/code/featuretools helper functions.R
@@ -20,42 +20,77 @@ filter_by_date <- function(data, cutoff_dt) {
   )
 }
 
+filter_by_date_all <- function(datalist, cutoff_dt) {
+  lapply(datalist, FUN = filter_by_date, cutoff_dt = cutoff_dt)
+}
+
 # Make an entity set from a list of dataframes
 
-entityset_create_entities <- function(df, df_name, debug = FALSE) {
-  if (debug) {print(paste('Adding', df_name))}
-  r_entityset %>%
-    add_entity(
-      df
-      , entity_id = df_name
-      , make_index = TRUE
-      , index = paste0(df_name, '_idx')
-      , time_index = attr(df, 'time_index')
-    )
+entityset_create_entities <- function(entityset_name, df, df_name, master_entity = 'households', master_idx = 'HOUSEHOLD_ID', debug = FALSE) {
+  if (debug) {print(paste('Adding', df_name, 'to', entityset_name))}
+  entityset <- get(entityset_name, envir = .GlobalEnv)
+  # Do not create an index for the master entity
+  if (df_name == master_entity) {
+    entityset %>%
+      add_entity(
+        df
+        , entity_id = master_entity
+        , index = master_idx
+        , time_index = attr(df, 'time_index')
+      )
+  } else {
+    entityset %>%
+      add_entity(
+        df
+        , entity_id = df_name
+        , make_index = TRUE
+        , index = paste0(df_name, '_idx')
+        , time_index = attr(df, 'time_index')
+      )
+  }
+  assign(entityset_name, entityset, envir = .GlobalEnv)
 }
 
-entityset_create_relationships <- function(df_name, debug = FALSE) {
-  if (debug) {print(df_name)}
-  r_entityset %>%
+entityset_create_relationships <- function(entityset_name, df_name, master_entity = 'households', master_idx = 'HOUSEHOLD_ID', debug = FALSE) {
+  if (debug) {paste(df_name, '->', master_entity, 'on', master_idx) %>% print()}
+  entityset <- get(entityset_name, envir = .GlobalEnv)
+  entityset %>%
     add_relationship(
-      parent_set = 'households'
+      parent_set = master_entity
       , child_set = df_name
-      , parent_idx = 'HOUSEHOLD_ID'
-      , child_idx = 'HOUSEHOLD_ID'
+      , parent_idx = master_idx
+      , child_idx = master_idx
     )
+  assign(entityset_name, entityset, envir = .GlobalEnv)
 }
 
-entityset_create <- function(entityset, datalist, cutoff_dt = NULL) {
+entityset_create <- function(entityset_name, datalist, master_entity = 'households', master_idx = 'HOUSEHOLD_ID', cutoff_dt = NULL, debug = FALSE) {
+  # Create new entityset
+  entityset <- create_entityset(id = entityset_name)
+  assign(entityset_name, entityset, envir = .GlobalEnv)
+  if (debug) {paste('New entity set:', entityset_name) %>% print()}
+  # Data cleanup
   if (is.null(cutoff_dt)) {cutoff_dt <- lubridate::ymd('99991231')}
-  datalist <- datalist %>% filter_by_date (cutoff_dt)
-  entityset <- mapply(
+  if (debug) {paste('cutoff date', cutoff_dt) %>% print()}
+  datalist <- datalist %>% filter_by_date_all(cutoff_dt)
+  if (debug) {paste('datalist size', length(datalist)) %>% print()}
+  # Create entities
+  mapply(
     entityset_create_entities
+    , entityset = entityset_name
     , df = datalist
     , df_name = names(datalist)
+    , master_entity = master_entity
+    , master_idx = master_idx
+    , debug = debug
   )[[1]]
+  # Create relationships
   entityset <- sapply(
     datalist %>% names()
-    , FUN = make_relationships
+    , FUN = entityset_create_relationships
+    , entityset_name = entityset_name
+    , master_entity = master_entity
+    , master_idx = master_idx
+    , debug = debug
   )[[1]]
-  return(entityset)
 }
